08d7d5ec37ecb2fd0b51d3b2d1def1eb8ecfe174
Francesco Oteri <francesco.oteri@gmail.com>
2024-04-14 18:38:58 +0200

    fix: typo

--- src/vs-app/src/App.vue
+++ src/vs-app/src/App.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import SubmissionForm from './components/SubmissionForm.vue'
+import SubmissionForm from '@/components/SubmissionForm.vue';
 </script>
 
 <template>
-<SubmissionForm />
+    <SubmissionForm />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
