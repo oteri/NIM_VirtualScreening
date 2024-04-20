6861a544810700dfecc82df7e83c9c2b7c43d2cb
Francesco Oteri <francesco.oteri@gmail.com>
2024-04-21 00:05:59 +0200

    refactor: remove rdkit dependency

--- src/vs-app/src/main.ts
+++ src/vs-app/src/main.ts
@@ -5,15 +5,3 @@ import './assets/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 createApp(App).mount('#app')
-
-declare global {
-    interface Window {
-        RDKit: RDKitModule
-    }
-}
-window.initRDKitModule()
-.then((instance: RDKitModule) => {
-    // window.RDKit is loaded and will be typed as RDKitModule
-    window.RDKit = instance;
-    return instance;
-})
